3cfd94530b644898c8d7803d8fe4d0137ead6be9
Nikh-on-Linux <nikhilhegde1011@gmail.com>
2026-07-19 19:47:16 +0530

    clean up

--- system-app/components/ui/flowcard.ui.component.tsx
+++ system-app/components/ui/flowcard.ui.component.tsx
@@ -5,7 +5,7 @@ function FlowCard({leftAlign,imageurl,title,body}:{leftAlign:Boolean,imageurl:St
   return (
     <div className={`${leftAlign ? "flex-row" : "flex-row-reverse"} w-full py-[55px] px-[28px] flex items-center gap-30 justify-between`} >
         <div className=' p-6 flex items-center justify-center texturebackground' >
-            <img src={imageurl} className='rounded-2xl' alt="" />
+            <img src={`${imageurl}`} className='rounded-2xl' alt="" />
         </div>
         <div className='w-[50%] flex flex-col gap-4 justify-center ' >
             <h2 className='font-newsreader text-4xl leading-9' >{title}</h2>
